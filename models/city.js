897043b240db568738e5a0c1b6ca1cf2f2e5c1ab
bobpac <141352390+bobpac@users.noreply.github.com>
2023-10-25 20:47:52 -0400

    Change DB to match actual API response

--- models/city.js
+++ models/city.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const citySchema = new Schema({
-    zipCode: { type: String, required: true },
-    countryCode: { type: String, required: true },
-    latitude: { type: String, required: true },
-    longitude: { type: String, required: true },
+    postal_code: { type: String, required: true },
+    country_code: { type: String, required: true },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
     city: { type: String, required: true },
     state: { type: String, required: true },
-    stateCode: { type: String, required: true },
+    state_code: { type: String, required: true },
     user: { 
         type: Schema.Types.ObjectId,
         ref: 'User',
